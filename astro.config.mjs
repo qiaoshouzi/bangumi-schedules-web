cfe70a605bca51ee9ae796983dd05a30bb40f26f
qiaoshouzi <qiaoshouzi@cfm.moe>
2026-03-21 20:53:57 +0800

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +7,4 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-});
+})
